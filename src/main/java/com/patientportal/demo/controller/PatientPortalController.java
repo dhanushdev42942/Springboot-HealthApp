3b72f2b0a25e838c1d73652df6b1d145cb0883c7
dhanushdev42942 <117530020+dhanushdev42942@users.noreply.github.com>
2022-11-05 15:35:50 -0400

    identity

--- src/main/java/com/patientportal/demo/controller/PatientPortalController.java
+++ src/main/java/com/patientportal/demo/controller/PatientPortalController.java
@@ -1,32 +1,25 @@
 package com.patientportal.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.patientportal.demo.model.AppointmentInput;
+import com.patientportal.demo.model.ProviderInput;
+import com.patientportal.demo.model.User;
+import com.patientportal.demo.model.UserInput;
 import com.patientportal.demo.repository.PatientPortalRepository;
-import com.patientportal.demo.model.*;
 
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class PatientPortalController {
 	
-	@Autowired
+	@Autowired(required=true)
 	PatientPortalRepository patientPortalRepository;
 	
 	@PostMapping("/userappointments")
@@ -53,14 +46,9 @@ public class PatientPortalController {
 	}
 	
 	@PostMapping("/createUser")
-	public ResponseEntity<?> createTutorial(@RequestBody User user) {
-		return null;
-//		try {
-//			Appointment appointment = 
-//			return new ResponseEntity<>(, HttpStatus.CREATED);
-//		} catch (Exception e) {
-//			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//		}
+	public ResponseEntity<?> createUser(@RequestBody User user) {
+	
+//		
 	}
 	
 	
--- src/main/java/com/patientportal/demo/model/User.java
+++ src/main/java/com/patientportal/demo/model/User.java
@@ -3,7 +3,7 @@ package com.patientportal.demo.model;
 import javax.persistence.Entity;
 
 import javax.persistence.GeneratedValue;
-
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
@@ -23,11 +23,8 @@ import lombok.NoArgsConstructor;
 public class User {
 
 	@Id
-
-	@GeneratedValue
-
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int userId;
-
 	String firstName;
 
 	String lastName;
